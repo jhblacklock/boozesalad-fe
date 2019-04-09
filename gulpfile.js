abdacdc1e8c56c775c424af5c79e731c9b2d84aa
Jackson Blacklock <jblacklock@truecar.com>
2019-04-09 11:41:39 -0500

    removed vendorized boostrap

--- gulpfile.js
+++ gulpfile.js
@@ -12,17 +12,10 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 
-// Load package.json for banner
-const pkg = require('./package.json');
-
-// Set the banner content
-const banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
+var bootstrapConfig = {
+    bootstrapDir: './bower_components/bootstrap-sass',
+    publicDir: './scss',
+};
 
 // BrowserSync
 function browserSync(done) {
@@ -48,9 +41,6 @@ function clean() {
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
-  // Bootstrap
-  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
   // Font Awesome
   var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
     .pipe(gulp.dest('./vendor'));
@@ -68,9 +58,21 @@ function modules() {
     .pipe(gulp.dest('./vendor/simple-line-icons/fonts'));
   var simpleLineIconsCSS = gulp.src('./node_modules/simple-line-icons/css/**')
     .pipe(gulp.dest('./vendor/simple-line-icons/css'));
-  return merge(bootstrap, fontAwesome, jquery, jqueryEasing, simpleLineIconsFonts, simpleLineIconsCSS);
+  return merge(fontAwesome, jquery, jqueryEasing, simpleLineIconsFonts, simpleLineIconsCSS);
 }
 
+function bootstrapCss() {
+  return gulp.src('./scss/app.scss')
+    .pipe(sass({
+        includePaths: [bootstrapConfig.bootstrapDir + '/assets/stylesheets'],
+    }))
+    .pipe(gulp.dest(bootstrapConfig.publicDir + '/css'));
+}
+
+function bootstrapFonts() {
+  return gulp.src(bootstrapConfig.bootstrapDir + '/assets/fonts/**/*')
+    .pipe(gulp.dest(bootstrapConfig.publicDir + '/fonts'));
+}
 // CSS task
 function css() {
   return gulp
@@ -84,9 +86,6 @@ function css() {
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
     }))
     .pipe(gulp.dest("./css"))
     .pipe(rename({
@@ -105,7 +104,7 @@ function watchFiles() {
 
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, css);
+const build = gulp.series(vendor, bootstrapCss, bootstrapFonts, css);
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
